7aed7617c10b8316efcb6d55d3e6a0ad50145cf5
Meryem Ben Jouda <miryambjzidi@gmail.com>
2022-08-25 15:48:15 +0200

    user model done

---
+++ server/models/User.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    String,
+    required: [true, "Please provide a username."],
+  },
+  password: {
+    String,
+    required: [true, "Please provide a password."],
+  } /* ,
+  email: {
+    String,
+    required: [true, "Please provide an email."]
+
+  } */,
+  image: {
+    String,
+    default: "https://pbs.twimg.com/media/FE-7qY8WUAATlNV.jpg",
+  },
+
+  favoriteArtist: {
+    type: [Schema.Types.ObjectId],
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
