5e7463274ac61bb866a4db63aec37b94ce07abb9
Chlotte <charlottealleaume99@gmail.com>
2022-08-25 14:40:02 +0200

    made the artist model

---
+++ server/models/Artist.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const artistSchema = new Schema({
+  'name': {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide the artist\'s name.']
+  },
+  'picture': {
+    type: String,
+    required: [true, 'Please provide a link to the artist\'s picture.']
+  },
+  'mini-bio': {
+    type: String,
+    required: [true, 'Please provide a bio of the artist.']
+  },
+  'albums': {
+    type: [Schema.Types.ObjectId],
+    ref: 'Album',
+    required: [true, 'Please provide a list of albums.']
+  },
+  'Flag song': {
+    type: String,
+    required: [true, 'Please provide a link to one of the artist\'s songs.']
+  },
+  'creatorId': {
+    type: [Schema.Types.ObjectId],
+    ref: 'User',
+  }
+});
+
+const Artist = model('Artist', artistSchema);
+
+module.exports = Artist;
