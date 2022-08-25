419c5b07a0e43699e4aa9ee5f9973592cd7e7706
Chlotte <charlottealleaume99@gmail.com>
2022-08-25 16:39:44 +0200

    re-re-organized working dir

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -4,12 +4,4 @@ router.get('/', (req, res, next) => {
   res.sendFile('index');
 });
 
-// Authentication routes
-router.use('/auth', require('./auth.routes'));
-
-// DB admin routes
-router.use('/auth', require('./auth.routes'));
-
-router.use('/auth', require('./auth.routes'));
-
 module.exports = router;
