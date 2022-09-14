8e1ca69b223ef24ade9d114c3ae011ab811b4eca
Chlotte <charlottealleaume99@gmail.com>
2022-09-14 18:37:30 +0200

    enabled CORS requests

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 
 // messages in the terminal as requests are coming in
 const logger = require('morgan')
@@ -7,6 +8,12 @@ const logger = require('morgan')
 module.exports = (app) => {
   // In development environment the app logs
   app.use(logger('dev'))
+
+  app.use(
+    cors({
+      origin: ['http://localhost:3000']
+    })
+  );
 
   // To have access to `body` property in the request
   app.use(express.json())
