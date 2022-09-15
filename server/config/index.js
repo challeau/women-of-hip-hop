6b5608691ca92b4c932781ff8d6738077c6cbcb7
Chlotte <charlottealleaume99@gmail.com>
2022-09-15 10:47:00 +0200

    still trying to enable cors

--- server/config/index.js
+++ server/config/index.js
@@ -5,28 +5,28 @@ const cors = require('cors');
 const logger = require('morgan');
 
 // Middleware configuration
-module.exports = (app) => {
-  // In development environment the app logs
-  app.use(logger('dev'));
+// module.exports = (app) => {
+//   // In development environment the app logs
+//   app.use(logger('dev'));
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
+//   app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested, Content-Type, Accept Authorization"
+//     )
+//     if (req.method === "OPTIONS") {
+//       res.header(
+//         "Access-Control-Allow-Methods",
+//         "POST, PUT, PATCH, GET, DELETE"
+//       )
+//       return res.status(200).json({})
+//     }
+//     next()
+//   })
   
   // app.set("trust proxy", 1);
-  // app.use(cors());
+  app.use(cors());
 
   // To have access to `body` property in the request
   app.use(express.json());
