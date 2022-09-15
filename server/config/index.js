c8104cd7be3d37208750174e3303fa615d4e6ab7
Chlotte <charlottealleaume99@gmail.com>
2022-09-15 11:35:27 +0200

    still trying to enable cors

--- server/config/index.js
+++ server/config/index.js
@@ -4,28 +4,11 @@ const cors = require('cors');
 // messages in the terminal as requests are coming in
 const logger = require('morgan');
 
-Middleware configuration
+// Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
   app.use(logger('dev'));
-
-//   app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested, Content-Type, Accept Authorization"
-//     )
-//     if (req.method === "OPTIONS") {
-//       res.header(
-//         "Access-Control-Allow-Methods",
-//         "POST, PUT, PATCH, GET, DELETE"
-//       )
-//       return res.status(200).json({})
-//     }
-//     next()
-//   })
   
-  // app.set("trust proxy", 1);
   app.use(cors({
       origin: ['http://localhost:3000']  // <== URL of our future React app
     }));
